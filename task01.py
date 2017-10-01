ebf9b468db81251a4125c8ab1fbc8772030849af
hexcola <hexcola@gmail.com>
2017-10-01 20:23:21 +0800

    update

--- task01.py
+++ task01.py
@@ -2,8 +2,16 @@
 import re
 from collections import Counter
 
+
 def dualPhraseGen(fragment):
     """ 根据句子片段生成二元词组列表.
+
+    Args:
+        fragment: 例如 `动物 只要 不患 疾病`，不含标点符号，且句中至少包含一个空格的句子片段
+    
+    Returns:
+        list: 二元词组列表. 例如：[('使', '自己'), ('自己', '更'), ('更', '善于')]
+
     """
     phrases = fragment.split(' ')
     dualPhrases = []
@@ -25,7 +33,7 @@ try:
         for line in lines:
             
             # 由于对[二元词组] 的定义不是很清楚，比如：二元词组必须有意义？ 
-            # 如果必须有意义，则不能称为基本测试了。但诸如 [的 ，] 或 [“ 我] 组合肯定不能算作二元词组的。
+            # 但诸如 [的 ，] 或 [“ 我] 组合肯定不能算作二元词组的。
             # 因此这里，仅将 章节名称或段落按标点符号分隔成片段。
             for fragment in re.split(u"[\.\!\/_,$%^*:(+\"\']+|[+——！，。？、~@#￥%……&*（）：；“”‘’《》―]+", line):
 
